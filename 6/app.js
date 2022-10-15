2a2b48cdbd21a194d954448d6d19060dd2a40427
wonjoon <zzzdnjswns@naver.com>
2022-10-15 11:10:42 +0900

    middleware

--- 6/app.js
+++ 6/app.js
@@ -3,11 +3,48 @@ const morgan = require("morgan");
 const cookieParser = require("cookie-parser");
 const app = express();
 const path = require("path");
+const session = require("express-session");
+const dotenv = require("dotenv");
 
+dotenv.config();
 app.set("port", process.env.PORT || 3000);
 
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/index.html"));
+app.use(morgan("dev"));
+app.use("/", express.static(path.join(__dirname, "public")));
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser(process.env.COOKIE_SECRET));
+app.use(
+  session({
+    resave: false,
+    saveUninitialized: false,
+    secret: process.env.COOKIE_SECRET,
+    cookie: {
+      httpOnly: true,
+      secure: false,
+    },
+    name: "session-cookie",
+  })
+);
+app.use((req, res, next) => {
+  console.log("모든 요청에 다 실행됩니다.");
+  next();
+});
+
+app.get(
+  "/",
+  (req, res, next) => {
+    console.log("Get / 요청에서만 실행됩니다.");
+    next();
+  },
+  (req, res) => {
+    throw new Error("에러는 에러 처리 미들웨어로 갑니다.");
+  }
+);
+
+app.use((err, req, res, next) => {
+  console.error(err);
+  res.status(500).send(err.message);
 });
 
 app.listen(app.get("port"), () => {
