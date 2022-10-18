91ca3aec194da2f4b5ca641967dc2ddf312a0be7
wonjoon <zzzdnjswns@naver.com>
2022-10-18 16:56:29 +0900

    20221018

--- 7/learn-sequelize/app.js
+++ 7/learn-sequelize/app.js
@@ -3,21 +3,19 @@ const path = require("path");
 const morgan = require("morgan");
 const nunjucks = require("nunjucks");
 
-const { sequelize } = require("./models");
+const { sequelize } = require("sequelize");
 
 const app = express();
-
-app.set("port", process.env.PORT || 3001);
+app.set("port", process.env.PORT || 3000);
 app.set("view engine", "html");
 nunjucks.configure("views", {
   express: app,
   watch: true,
 });
-
 sequelize
   .sync({ force: false })
   .then(() => {
-    console.log("데이터 베이스 연결 성공");
+    console.log("데이터베이스 연결 성공");
   })
   .catch((err) => {
     console.error(err);
@@ -29,18 +27,18 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
 app.use((req, res, next) => {
-  const error = new Error(`${req.method} ${req.url} 라우터가 업습니다.`);
-  error.status = 404;
+  const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
+  error.status(404);
   next(error);
 });
 
-app.use((err, req, res, next) => {
+app.use((err, req, res, mext) => {
   res.locals.message = err.message;
-  res.locals.err = process.env.NODE_ENV !== "product" ? err : {};
+  res.locals.error = process.env.NODE_ENV !== "production" ? err : {};
   res.status(err.status || 500);
   res.render("error");
 });
 
 app.listen(app.get("port"), () => {
-  console.log(app.get("port"), "번 포트에서 대기 중");
+  console.log(app.get("port"), "번 포트에서 대기중");
 });
