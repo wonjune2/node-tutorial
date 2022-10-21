868233d5de7b42956e2e706f895915281ec141ed
wonjoon <zzzdnjswns@naver.com>
2022-10-21 20:23:25 +0900

    crud 작업

--- 7/learn-sequelize/app.js
+++ 7/learn-sequelize/app.js
@@ -3,6 +3,10 @@ const path = require("path");
 const morgan = require("morgan");
 const nunjucks = require("nunjucks");
 
+const indexRouter = require("./routes");
+const usersRouter = require("./routes/users");
+const commentsRouter = require("./routes/comments");
+
 const { sequelize } = require("./models");
 
 const app = express();
@@ -25,6 +29,10 @@ app.use(morgan("dev"));
 app.use(express.static(path.join(__dirname, "public")));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/comments", commentsRouter);
 
 app.use((req, res, next) => {
   const error = new Error(`${req.method} ${req.url} 라우터가 없습니다.`);
